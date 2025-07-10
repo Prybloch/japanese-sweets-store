f2289e1469c0958470c7c4f492ad5fde60363c33
Prybloch <prybloch@gmail.com>
2025-07-10 12:30:10 -0300

    Fix bugs pt1

--- script.js
+++ script.js
@@ -5,35 +5,39 @@ let listItemDom = document.querySelector(".carousel .carousel-container");
 let thumbnailDom = document.querySelector(".carousel .thumbnail");
 
 nextItem.onclick = function () {
-  showSlider('next');
-}
+  showSlider("next");
+};
 
 prevItem.onclick = function () {
-  showSlider('prev');
-}
+  showSlider("prev");
+};
 
 let runTimeOut;
 
 function showSlider(type) {
-    let itemSlider = document.querySelectorAll(".carousel .carousel-container .carousel-items");
-    let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .carousel-items");
+  let itemSlider = document.querySelectorAll(
+    ".carousel .carousel-container .carousel-items"
+  );
+  let itemThumbnail = document.querySelectorAll(
+    ".carousel .thumbnail .carousel-items"
+  );
 
-    if( type === 'next') {
-        listItemDom.appendChild(itemSlider[0]);
-        thumbnailDom.appendChild(itemThumbnail[0]);
-        carouselDom.classList.add('next');
-    }
-    else {
-      let positionLastItem = itemSlider.length - 1;
-      listItemDom.prepend(itemSlider[positionLastItem]);
-      thumbnailDom.prepend(itemThumbnail[positionLastItem])
-      carouselDom.classList.add('prev');
-    }
+  if (type === 'next') {
+    carouselDom.classList.add("next");
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+  }
+   else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add("prev");
+  }
 
-    clearTimeout()
+  clearTimeout(runTimeOut);
 
-    runTimeOut = setTimeout(() => {
-      carouselDom.classList.remove('next')
-      carouselDom.classList.remove('prev')
-    },1000);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, 1000);
 }
